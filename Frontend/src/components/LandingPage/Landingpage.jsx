17a9015adf83bdb3a942e98818b4b452d02b4287
Deepak <naagadeepakkumar2000@gmail.com>
2026-04-23 14:53:33 +0530

    deepak

--- Frontend/src/components/LandingPage/Landingpage.jsx
+++ Frontend/src/components/LandingPage/Landingpage.jsx
@@ -2,14 +2,15 @@ import React from 'react'
 import HeroSection from './HeroSection'
 import PopularPackages from './PopularPackages'
 import Navbar from './Navbar'
+import { Box, Button, Chip, Container, Stack, TextField, Typography } from '@mui/material'
 
 
 function Landingpage() {
   return (
     <div>
         <Navbar/>
-        {/* <HeroSection/>
-        <PopularPackages/> */}
+        <HeroSection/>
+        <PopularPackages/>
     </div>
   )
 }
