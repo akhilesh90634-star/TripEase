f57bc09c6fe6b7c00cc0f99259fc3f6dea42f059
akhilesh90634-star <akhilesh90634@gmail.com>
2026-04-23 23:39:03 +0530

    login and Register with be is done and added to branch(Akhilesh)

--- Frontend/src/components/SignupPages/Login.jsx
+++ Frontend/src/components/SignupPages/Login.jsx
@@ -1,3 +1,230 @@
+// import React, { useState } from "react";
+// import {
+//   Box,
+//   TextField,
+//   Button,
+//   Typography,
+//   Card,
+//   Link,
+//   Grid,
+//   IconButton,
+//   InputAdornment,
+//   Snackbar,
+//   Alert,
+// } from "@mui/material";
+// import { useNavigate } from "react-router-dom";
+// import { Visibility, VisibilityOff } from "@mui/icons-material";
+// import Navbar from "../LandingPage/Navbar";
+// import api from "../Api/Api";
+
+// function Login() {
+//   const navigate = useNavigate();
+//   const [data, setData] = useState({ email: "", password: "" });
+//   const [showPassword, setShowPassword] = useState(false);
+//   const [open, setOpen] = useState(false);
+//   const [msg, setMsg] = useState("");
+//   const [type, setType] = useState("error");
+
+//   function handleChange(e) {
+//     setData({
+//       ...data,
+//       [e.target.name]: e.target.value,
+//     });
+//   }
+
+//   function handleLogin() {
+//     if (!data.email || !data.password) {
+//       setMsg("Please fill the details");
+//       setType("error");
+//       setOpen(true);
+//       return;
+//     }
+
+//    const result = res.data;
+
+//       localStorage.setItem("accessToken", result.accessToken);
+//       localStorage.setItem("refreshToken", result.refreshToken);
+//       localStorage.setItem("role", result.role);
+
+//     // clear form
+//     setData({
+//       email: "",
+//       password: "",
+//     });
+
+//     setShowPassword(false);
+//   }
+
+//   return (
+//     <div>
+//       <Navbar />
+//       <Box
+//         sx={{
+//           paddingTop:"90px",
+//           minHeight: "88vh",
+//           pt: { xs: 6, md: 0, lg: 11 },
+//           backgroundImage: `
+//           linear-gradient(rgba(0,0,0,0.3), rgba(0,0,0,0.3)),
+//           url('/login.png')
+//         `,
+//           backgroundSize: "cover",
+//           backgroundPosition: {
+//             xs: "center",
+//             md: "center",
+//           },
+//         }}
+//       >
+//         <Grid container sx={{ height: "82vh" }}>
+//           {/* LEFT SIDE TEXT */}
+
+//           <Grid
+//             item
+//             xs={12}
+//             md={6}
+//             sx={{
+//               display: "flex",
+//               flexDirection: "column",
+//               justifyContent: "center",
+//               px: 8,
+//               color: "#fff",
+//               textAlign: { xs: "center", md: "left" },
+//             }}
+//           >
+//             <Typography
+//               variant="h3"
+//               fontWeight="bold"
+//               sx={{
+//                 textShadow: "2px 2px 10px rgba(0,0,0,0.7)",
+//               }}
+//             >
+//               Welcome To{" "}
+//               <Box component="span" sx={{ fontStyle: "italic" }}>
+//                 TripEase
+//               </Box>
+//             </Typography>
+
+//             <Typography
+//               variant="h6"
+//               sx={{
+//                 mt: 2,
+//                 maxWidth: 400,
+//                 fontStyle: "italic",
+//               }}
+//             >
+//               Login to explore destinations, <br />
+//               manage your trips, and travel with ease.
+//             </Typography>
+//           </Grid>
+
+//           {/* RIGHT SIDE LOGIN */}
+//           <Grid
+//             item
+//             xs={12}
+//             md={6}
+//             sx={{
+//               display: "flex",
+//               flexDirection: "row",
+//               flexWrap: "wrap",
+//               alignItems: "center",
+//               justifyContent: "center",
+//               alignContent: "center",
+//               gap: "0px", // same as row/column spacing = 0
+//               minWidth: 0,
+//               boxSizing: "border-box",
+//               height: "100%",
+
+//               mt: { xs: 3, md: 6 },
+//               mb: { xs: 3, md: 0 },
+//               px: 2,
+//             }}
+//           >
+//             <Card
+//               sx={{
+//                 p: 3,
+//                 width: 350,
+//                 borderRadius: 4,
+//                 backgroundColor: "rgba(255,255,255,0.9)",
+//                 backdropFilter: "blur(20px)",
+//                 boxShadow: "0 8px 30px rgba(0,0,0,0.3)",
+//               }}
+//             >
+//               <Typography
+//                 variant="h5"
+//                 fontWeight="bold"
+//                 textAlign="center"
+//                 mb={1}
+//               >
+//                 Login
+//               </Typography>
+
+//               <TextField
+//                 fullWidth
+//                 label="Email"
+//                 name="email"
+//                 margin="normal"
+//                 value={data.email}
+//                 onChange={handleChange}
+//               />
+
+//               <TextField
+//                 fullWidth
+//                 label="Password"
+//                 type={showPassword ? "text" : "password"}
+//                 name="password"
+//                 margin="normal"
+//                 value={data.password}
+//                 onChange={handleChange}
+//                 InputProps={{
+//                   endAdornment: (
+//                     <InputAdornment position="end">
+//                       <IconButton
+//                         onClick={() => setShowPassword(!showPassword)}
+//                         edge="end"
+//                       >
+//                         {showPassword ? <VisibilityOff /> : <Visibility />}
+//                       </IconButton>
+//                     </InputAdornment>
+//                   ),
+//                 }}
+//               />
+
+//               <Button
+//                 fullWidth
+//                 variant="contained"
+//                 sx={{ mt: 3, mb: 2, borderRadius: 2 }}
+//                 onClick={handleLogin}
+//               >
+//                 Login
+//               </Button>
+
+//               <Typography textAlign="center">
+//                 Don't have an account?{" "}
+//                 <Link component="button" onClick={() => navigate("/register")}>
+//                   Register
+//                 </Link>
+//               </Typography>
+//             </Card>
+//           </Grid>
+//         </Grid>
+
+//         {/* Snackbar */}
+//         <Snackbar
+//           open={open}
+//           autoHideDuration={5000}
+//           onClose={() => setOpen(false)}
+//           anchorOrigin={{ vertical: "top", horizontal: "right" }}
+//         >
+//           <Alert severity={type} onClose={() => setOpen(false)}>
+//             {msg}
+//           </Alert>
+//         </Snackbar>
+//       </Box>
+//     </div>
+//   );
+// }
+
+// export default Login;
+
 import React, { useState } from "react";
 import {
   Box,
@@ -32,7 +259,7 @@ function Login() {
     });
   }
 
-  function handleLogin() {
+  async function handleLogin() {
     if (!data.email || !data.password) {
       setMsg("Please fill the details");
       setType("error");
@@ -40,19 +267,44 @@ function Login() {
       return;
     }
 
-   const result = res.data;
+    try {
+      let res;
+
+      try {
+        res = await api.post("/admin/login", data);
+      } catch {
+        try {
+          res = await api.post("/agent/login", data);
+        } catch {
+          res = await api.post("/user/login", data);
+        }
+      }
+
+      const result = res.data;
 
       localStorage.setItem("accessToken", result.accessToken);
       localStorage.setItem("refreshToken", result.refreshToken);
       localStorage.setItem("role", result.role);
 
-    // clear form
-    setData({
-      email: "",
-      password: "",
-    });
+      if (result.role === "admin") {
+        navigate("/admin");
+      } else if (result.role === "agent") {
+        navigate("/agent");
+      } else {
+        navigate("/customer");
+      }
 
-    setShowPassword(false);
+      setData({
+        email: "",
+        password: "",
+      });
+
+      setShowPassword(false);
+    } catch (err) {
+      setMsg("Invalid credentials");
+      setType("error");
+      setOpen(true);
+    }
   }
 
   return (
@@ -60,13 +312,13 @@ function Login() {
       <Navbar />
       <Box
         sx={{
-          paddingTop:"90px",
+          paddingTop: "90px",
           minHeight: "88vh",
           pt: { xs: 6, md: 0, lg: 11 },
           backgroundImage: `
-          linear-gradient(rgba(0,0,0,0.3), rgba(0,0,0,0.3)),
-          url('/login.png')
-        `,
+            linear-gradient(rgba(0,0,0,0.3), rgba(0,0,0,0.3)),
+            url('/login.png')
+          `,
           backgroundSize: "cover",
           backgroundPosition: {
             xs: "center",
@@ -75,8 +327,7 @@ function Login() {
         }}
       >
         <Grid container sx={{ height: "82vh" }}>
-          {/* LEFT SIDE TEXT */}
-
+          {/* LEFT SIDE */}
           <Grid
             item
             xs={12}
@@ -116,7 +367,7 @@ function Login() {
             </Typography>
           </Grid>
 
-          {/* RIGHT SIDE LOGIN */}
+          {/* RIGHT SIDE */}
           <Grid
             item
             xs={12}
@@ -128,11 +379,10 @@ function Login() {
               alignItems: "center",
               justifyContent: "center",
               alignContent: "center",
-              gap: "0px", // same as row/column spacing = 0
+              gap: "0px",
               minWidth: 0,
               boxSizing: "border-box",
               height: "100%",
-
               mt: { xs: 3, md: 6 },
               mb: { xs: 3, md: 0 },
               px: 2,
